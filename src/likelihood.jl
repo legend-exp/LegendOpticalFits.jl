a292bbffb30f07a066595038410853abc47957f9
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-09-16 18:09:49 +0200

    update likelihood function

--- src/likelihood.jl
+++ src/likelihood.jl
@@ -8,7 +8,7 @@ We model the fraction of events with no detected light (`λ0`) as follows:
 - For each channel, the expected no-light probability `λ0_model` comes from
   the simulation (`log_p0_nominal`) combined with random coincidences
   (`x0_random_coin`) and scaled by per-channel scaling factors
-  (`params.epsilons`, the parameters of the model).
+  (the parameters of the model).
 
 - The observed no-light fraction in data is `λ0_data = N0 / N_data`, where `N0`
   is the number of no-light events passing a multiplicity threshold.
@@ -20,47 +20,48 @@ We model the fraction of events with no detected light (`λ0`) as follows:
 The likelihood is the sum of log-probabilities across all channels.
 
 # Arguments
-- `N_data`: number of selected data events after applying the multiplicity cut.
-- `λ0_data`: observed no-light fractions per channel from data.
+- `x0`: observed no-light indicators from data events.
 - `log_p0_nominal`: logarithm of the probability to see no light for each
   (event, channel), typically from simulations.
-- `x0_random_coin`: presence of light from random coincidences for each
-  (event, channel). This is typically coming from a measurement.
+- `x0_random_coin`: observed no-light indicators from random coincidence events.
 - `multiplicity_thr`: discard events with multiplicity below this threshold
   (optional, defaults to 0).
 
 # Returns
-- A `DensityFunction` object representing the log-likelihood.  It can be called
-  with a parameter set `params` that must include `params.epsilons`
+- A `DensityFunction` object representing the log-likelihood. It can be called
+  with a parameter set.
 """
 function make_λ0_likelihood(
-    N_data::Int,
-    λ0_data::Table,
+    x0::Table,
     log_p0_nominal::Table,
     x0_random_coin::Table,
     ;
-    multiplicity_thr::Int = 8
+    multiplicity_thr::Int = 0
 )
-    return logfuncdensity(
-        params -> begin
+    N_data = length(x0)
+    data = λ0_data(x0)
 
-            λ0_model = expected_λ0(
-                params.epsilons,
+    return DensityInterface.logfuncdensity(
+        params -> begin
+            model = λ0_model(
+                params,
                 log_p0_nominal,
-                x0_random_coin;
+                x0_random_coin,
                 multiplicity_thr = multiplicity_thr
             )
 
             logpmf = 0.0
-            @inbounds @simd for i in eachindex(λ0_model)
+            @inbounds @simd for i in eachindex(model)
                 # Binomal statistics
-                µ = λ0_model[i]
+                µ = model[i]
                 σ = sqrt(µ * (1 - µ) / N_data)
 
-                logpmf += logpdf(Normal(μ, σ), λ0_data[i])
+                logpmf += logpdf(Normal(μ, σ), data[i])
             end
 
             return logpmf
         end
     )
 end
+
+export make_λ0_likelihood
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ Test.@testset verbose=true "Package LegendOpticalFits" begin
     include("test_aqua.jl")
     include("test_data.jl")
     include("test_models.jl")
+    include("test_likelihood.jl")
     include("test_utils.jl")
     include("test_channelmap.jl")
     include("test_docs.jl")
